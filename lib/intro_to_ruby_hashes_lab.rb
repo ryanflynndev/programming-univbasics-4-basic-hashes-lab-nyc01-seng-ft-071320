e650eac12783d1b5f7ff5c18e69cb01bab58d80b
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-06 19:19:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,16 @@
 def new_hash
   # return an empty hash
+  hash = {}
+  
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hash = {
+    first_evolution: "abra",
+    second_evolution: "kadabra",
+    third_evolution: "alakazam"
+  }
 end
 
 def pioneer
