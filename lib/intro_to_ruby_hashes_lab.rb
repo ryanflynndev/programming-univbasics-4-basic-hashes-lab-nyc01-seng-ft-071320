9008107b88befa66f8ca54fb43b8f9726302edad
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-06 19:20:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,14 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+    name: 'Grace Hopper'
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash = {}
+  hash[number] = "Hey"
 end
